d9352ef3bada0165c0603b5cbf8f999a3209f130
M.M.A. van Spronsen <marc.van.spronsen@hva.nl>
2019-10-13 15:36:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/reminderapp/AddActivity.kt
@@ -0,0 +1,38 @@
+package com.example.reminderapp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_add.*
+import kotlinx.android.synthetic.main.content_add.*
+
+const val EXTRA_REMINDER = "EXTRA_REMINDER"
+
+class AddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener {
+            OnSaveClick()
+        }
+    }
+
+    private fun OnSaveClick() {
+        if (etAddReminder.text.toString().isNotBlank()) {
+            val reminder = Reminder(etAddReminder.text.toString())
+            val resultIntent = Intent()
+            resultIntent.putExtra(EXTRA_REMINDER, reminder)
+            setResult(Activity.RESULT_OK, resultIntent)
+            finish()
+        } else {
+            Toast.makeText(this, "Reminder can not be empty", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/reminderapp/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.example.reminderapp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_add.*
+import kotlinx.android.synthetic.main.content_main.*
+
+const val ADD_REMINDER_REQUEST_CODE = 100
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener {
+            startAddActivity()
+        }
+
+        InitViews()
+    }
+
+    private fun startAddActivity(){
+        val intent = Intent(this, AddActivity::class.java)
+        startActivityForResult(intent, ADD_REMINDER_REQUEST_CODE)
+    }
+
+    private val reminders = arrayListOf<Reminder>()
+    private val reminderAdapter = ReminderAdapter(reminders)
+
+    private fun InitViews() {
+        createItemTouchHelper().attachToRecyclerView(rvReminders)
+        rvReminders.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        rvReminders.adapter = reminderAdapter
+        rvReminders.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK){
+            when (requestCode) {
+                ADD_REMINDER_REQUEST_CODE -> {
+                    val reminder = data!!.getParcelableExtra<Reminder>(EXTRA_REMINDER)
+                    reminders.add(reminder)
+                    reminderAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+    }
+
+    private fun createItemTouchHelper() : ItemTouchHelper {
+        val callback = object: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                reminders.removeAt(position)
+                reminderAdapter.notifyDataSetChanged()
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
